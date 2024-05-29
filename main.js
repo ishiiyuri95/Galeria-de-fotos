0ac8dcf9d052a21b62e42c7f01c98afc1db8d09f
ishiiyuri95 <ishiiyuri@icloud.com>
2024-05-29 14:09:00 -0300

    ajuste

--- main.js
+++ main.js
@@ -10,15 +10,17 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li></li>');
+        const novoItem = $('<li style="display: none"></li>');
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
             <div class="overlay-imagem-link">
-            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                 Ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
 })
